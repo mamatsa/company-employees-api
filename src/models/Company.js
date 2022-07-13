2c2a21d3de655bb2db803078c63742bf248d5832
otomamatsashvili <otar.mamatsashvili@gmail.com>
2022-07-13 23:49:39 +0400

    feat: add company model

---
+++ src/models/Company.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+const companySchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  websiteAddress: {
+    type: String,
+    required: true,
+  },
+  logoAddress: {
+    type: String,
+    required: true,
+  },
+  establishmentDate: {
+    type: Date,
+    required: true,
+  },
+})
+
+const Company = mongoose.model('Company', companySchema)
+
+export default Company
