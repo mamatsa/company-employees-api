ad026a2cf0b77f6e81f8b2bbdb2bf7ff0b357bbe
otomamatsashvili <otar.mamatsashvili@gmail.com>
2022-07-13 23:57:13 +0400

    feat: add swagger

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import express from 'express'
+import { swaggerMiddleware } from './middlewares/index.js'
 import companyRoutes from './routes/company.js'
 import connectDB from './config/mongo.js'
 
@@ -11,5 +12,7 @@ server.use(express.json())
 server.use(express.urlencoded({ extended: false }))
 
 server.use('/company', companyRoutes)
+
+server.use('/api', swaggerMiddleware())
 
 server.listen(process.env.PORT)
