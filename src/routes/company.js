2a0f8e7e8116a563f622ab6442238341ba206466
otomamatsashvili <otar.mamatsashvili@gmail.com>
2022-07-16 17:27:37 +0400

    feat: protect private routes

--- src/routes/company.js
+++ src/routes/company.js
@@ -6,11 +6,16 @@ import {
   updateCompany,
   deleteCompany,
 } from '../controllers/company-controller.js'
+import { authMiddleware } from '../middlewares/index.js'
 
 const router = express.Router()
 
-router.route('/').get(getCompanyList).post(createCompany)
+router.route('/').get(getCompanyList).post(authMiddleware, createCompany)
 
-router.route('/:id').get(getCompany).put(updateCompany).delete(deleteCompany)
+router
+  .route('/:id')
+  .get(getCompany)
+  .put(authMiddleware, updateCompany)
+  .delete(authMiddleware, deleteCompany)
 
 export default router
