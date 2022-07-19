d5ad399c1692fe7a87c18f6111df04e15611d522
otomamatsashvili <otar.mamatsashvili@gmail.com>
2022-07-20 00:00:10 +0400

    refactor: dynamically build mongo uri from .env

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -3,7 +3,23 @@ import colors from 'colors'
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI)
+    let mongoUri
+    const {
+      MONGO_PROTOCOL,
+      MONGO_DATABASE,
+      MONGO_PORT,
+      MONGO_USER,
+      MONGO_PASSWORD,
+      MONGO_CLUSTER,
+      MONGO_PARAMS,
+      APP_URI,
+    } = process.env
+    if (MONGO_PROTOCOL === 'mongodb+srv') {
+      mongoUri = `${MONGO_PROTOCOL}://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_CLUSTER}.utytl.mongodb.net/${MONGO_DATABASE}?${MONGO_PARAMS}`
+    } else if (MONGO_PROTOCOL === 'mongodb') {
+      mongoUri = `${MONGO_PROTOCOL}://${APP_URI}:${MONGO_PORT}/${MONGO_DATABASE}`
+    }
+    const conn = await mongoose.connect(mongoUri)
     console.log(
       colors.underline.cyan(`MongoDB Connected: ${conn.connection.host}`)
     )
